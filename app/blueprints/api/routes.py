5f31bae61c52362f61f93aa430a61a9585fd36fb
vremes <valtteriremes@outlook.com>
2021-01-23 20:16:30 +0200

    Create new helpers instead of using multiple function calls.

--- app/blueprints/api/routes.py
+++ app/blueprints/api/routes.py
@@ -1,6 +1,4 @@
 import os
-import hmac
-import hashlib
 from app.helpers import files, auth
 from app.helpers.api import response
 from flask import Blueprint, request, abort, current_app, jsonify, url_for, render_template_string
@@ -9,23 +7,8 @@ api = Blueprint('api', __name__)
 
 @api.route('/sharex')
 def sharex():
-    response_dict = {
-        "Version": "1.0.0",
-        "DestinationType": "ImageUploader, FileUploader",
-        "RequestMethod": "POST",
-        "RequestURL": url_for('api.upload', _external=True),
-        "Body": "MultipartFormData",
-        "FileFormName": "file",
-        "URL": "$json:url$",
-        "DeletionURL": "$json:delete_url$",
-        "Headers": {
-            "Authorization": "YOUR-UPLOAD-PASSWORD-HERE",
-            "X-Use-Original-Filename": 1,
-        },
-        "ErrorMessage": "$json:status$"
-    }
-
-    return jsonify(response_dict)
+    sharex_config = files.sharex_config()
+    return jsonify(sharex_config)
 
 @api.route('/upload', methods=['POST'])
 @auth.auth_required
@@ -34,49 +17,46 @@ def upload():
     upload_directory = current_app.config['UPLOAD_DIR']
     use_og_filename = request.headers.get('X-Use-Original-Filename', type=int) == 1
 
+    # If client did not send a file, abort
     if uploaded_file is None:
         return response(400, 'File upload failed, invalid file')
 
-    # Check if upload directory exists
+    # Check if file is allowed, based on file extension
+    if files.is_allowed_file(uploaded_file.filename) is False:
+        return response(400, 'Invalid file type')
+
+     # Check if upload directory exists
     if os.path.isdir(upload_directory) is False:
         os.makedirs(upload_directory)
 
-    original_filename = uploaded_file.filename.lower()
-
-    # Check if file is allowed
-    if files.is_allowed_file(original_filename) is False:
-        return response(400, 'Invalid file type')
-
-    modified_filename = files.get_modified_filename(original_filename, use_og_filename)
+    modified_filename = files.get_modified_filename(uploaded_file.filename, use_og_filename)
 
     # Save file
     save_directory = os.path.join(upload_directory, modified_filename)
     uploaded_file.save(save_directory)
 
-    # Generate HMAC using Flask's secret key and filename
-    secret_key = current_app.secret_key.encode('utf-8')
-    hmac_data = modified_filename.encode('utf-8')
-    signature = hmac.new(secret_key, hmac_data,hashlib.sha256).hexdigest()
+    # Generate HMAC hash using Flask's secret key and filename
+    hmac_hash = files.create_hmac_hash(current_app.secret_key, modified_filename)
 
     return jsonify(
             {
                 'filename': modified_filename, 
                 'url': url_for('main.uploads', filename=modified_filename, _external=True),
-                'delete_url': url_for('api.delete_file', signature=signature, filename=modified_filename, _external=True)
+                'delete_url': url_for('api.delete_file', hmac_hash=hmac_hash, filename=modified_filename, _external=True)
             }
         )
 
-@api.route('/delete-file/<signature>/<filename>')
-def delete_file(signature, filename):
-    secret_key = current_app.secret_key.encode('utf-8')
-    hmac_data = filename.encode('utf-8')
-    hmac_signature = hmac.new(secret_key, hmac_data, hashlib.sha256).hexdigest()
+@api.route('/delete-file/<hmac_hash>/<filename>')
+def delete_file(hmac_hash, filename):
+    new_hmac_hash = files.create_hmac_hash(current_app.secret_key, filename)
 
-    if hmac.compare_digest(hmac_signature, signature) is False:
+    # If hash does not match
+    if files.is_valid_hash(hmac_hash, new_hmac_hash) is False:
         return abort(404)
 
     file_path = os.path.join(current_app.config['UPLOAD_DIR'], filename)
     
+    # If file does not exist
     if os.path.isfile(file_path) is False:
         return abort(404)
 
--- app/helpers/files.py
+++ app/helpers/files.py
@@ -1,4 +1,7 @@
 import os
+import hmac
+import flask
+import hashlib
 import secrets
 from app import config
 from werkzeug.utils import secure_filename
@@ -26,3 +29,44 @@ def get_modified_filename(filename: str, use_original_filename: bool = False) ->
         full_filename = '{}{}'.format(random_fname, ext)
 
     return full_filename
+
+def create_hmac_hash(secret, hmac_data) -> str:
+    """
+    Creates a hash using `hmac.new` and returns it
+
+    :param str secret: The secret key for the hash
+    :param str hmac_data: The message/input for the hash
+    :return str hmac_hash HMAC hash
+    """
+    secret = secret.encode('utf-8')
+    hmac_data = hmac_data.encode('utf-8')
+    hmac_hash = hmac.new(secret, hmac_data,hashlib.sha256).hexdigest()
+    return hmac_hash
+
+def is_valid_hash(hash_a: str, hash_b: str) -> bool:
+    """
+    Compares two hashes using `hmac.compare_digest`
+    """
+    return hmac.compare_digest(hash_a, hash_b)
+
+def sharex_config():
+    """
+    Returns the configuration for ShareX as dictionary
+    """
+    cfg = {
+        "Version": "1.0.0",
+        "DestinationType": "ImageUploader, FileUploader",
+        "RequestMethod": "POST",
+        "RequestURL": flask.url_for('api.upload', _external=True),
+        "Body": "MultipartFormData",
+        "FileFormName": "file",
+        "URL": "$json:url$",
+        "DeletionURL": "$json:delete_url$",
+        "Headers": {
+            "Authorization": "YOUR-UPLOAD-PASSWORD-HERE",
+            "X-Use-Original-Filename": 1,
+        },
+        "ErrorMessage": "$json:status$"
+    }
+
+    return cfg
