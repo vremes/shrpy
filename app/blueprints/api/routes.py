55d3b5ec5a8b8be1f2a1f4b72d67063a464a2fa7
vremes <valtteriremes@outlook.com>
2021-01-30 18:43:26 +0200

    Refactor files helper to a File class.

--- app/blueprints/api/routes.py
+++ app/blueprints/api/routes.py
@@ -1,13 +1,14 @@
 import os
 from app.helpers import files, auth
 from app.helpers.api import response
+from app.helpers.files import File
 from flask import Blueprint, request, abort, current_app, jsonify, url_for, render_template_string, safe_join
 
 api = Blueprint('api', __name__)
 
 @api.route('/sharex')
 def sharex():
-    sharex_config = files.sharex_config()
+    sharex_config = files.File.sharex_config()
     return jsonify(sharex_config)
 
 @api.route('/upload', methods=['POST'])
@@ -17,50 +18,41 @@ def upload():
     upload_directory = current_app.config['UPLOAD_DIR']
     use_og_filename = request.headers.get('X-Use-Original-Filename', type=int) == 1
 
-    # If client did not send a file, abort
-    if uploaded_file is None:
-        return response(400, 'File upload failed, invalid file')
+    # Our own class which utilises werkzeug.datastructures.FileStorage
+    f = File(uploaded_file, use_og_filename)
 
-    # Check if file is allowed, based on file extension
-    if files.is_allowed_file(uploaded_file.filename) is False:
+    # Check if file is allowed
+    if f.is_allowed() is False:
         return response(400, 'Invalid file type')
 
-     # Check if upload directory exists
-    if os.path.isdir(upload_directory) is False:
-        os.makedirs(upload_directory)
+    # Get the filename
+    filename = f.get_filename()
 
-    modified_filename = files.get_modified_filename(uploaded_file.filename, use_og_filename)
-
-    # Save file
-    save_directory = safe_join(upload_directory, modified_filename)
-    uploaded_file.save(save_directory)
+    # Save the file
+    f.save()
 
     # Generate HMAC hash using Flask's secret key and filename
-    hmac_hash = files.create_hmac_hash(current_app.secret_key, modified_filename)
+    hmac_hash = File.create_hmac_hash(filename, current_app.secret_key)
 
+    # Return JSON
     return jsonify(
             {
-                'filename': modified_filename, 
-                'url': url_for('main.uploads', filename=modified_filename, _external=True),
-                'delete_url': url_for('api.delete_file', hmac_hash=hmac_hash, filename=modified_filename, _external=True)
+                'filename': filename, 
+                'url': url_for('main.uploads', filename=filename, _external=True),
+                'delete_url': url_for('api.delete_file', hmac_hash=hmac_hash, filename=filename, _external=True)
             }
         )
 
 @api.route('/delete-file/<hmac_hash>/<filename>')
 def delete_file(hmac_hash, filename):
-    new_hmac_hash = files.create_hmac_hash(current_app.secret_key, filename)
+    new_hmac_hash = File.create_hmac_hash(filename, current_app.secret_key)
 
     # If hash does not match
-    if files.is_valid_hash(hmac_hash, new_hmac_hash) is False:
+    if File.is_valid_hash(hmac_hash, new_hmac_hash) is False:
         return abort(404)
 
-    file_path = safe_join(current_app.config['UPLOAD_DIR'], filename)
-    
-    # If file does not exist
-    if os.path.isfile(file_path) is False:
+    if File.delete(filename) is False:
         return abort(404)
-
-    os.remove(file_path)
     
     message = render_template_string('{{ filename }} has been deleted, you can now close this page', filename=filename)
     return response(message=message)
--- app/helpers/files.py
+++ app/helpers/files.py
@@ -1,70 +1,120 @@
 import os
 import hmac
 import flask
-import hashlib
 import secrets
+import hashlib
+
 from app import config
+from werkzeug.security import safe_join
 from werkzeug.utils import secure_filename
+from werkzeug.datastructures import FileStorage
 
-def is_allowed_file(filename: str) -> bool:
-    """
-    Check if `filename`'s extension is allowed
-    """
-    _, ext = os.path.splitext(filename.lower())
+class File:
+    def __init__(self, werkzeug_file_class: FileStorage, use_original_filename = False):
+        """
+        Class for uploaded files which takes the `werkzeug.datastructures.FileStorage` from `flask.Request.files` as first parameter
 
-    return ext in config.ALLOWED_EXTENSIONS
+        :param werkzeug_file_class 
+        """
 
-def get_modified_filename(filename: str, use_original_filename: bool = False) -> str:
-    """
-    Returns modified filename
-    """
-    fname, ext = os.path.splitext(filename.lower())
-    secure_fname = secure_filename(fname)
-    random_fname = secrets.token_urlsafe(12)
+        if isinstance(werkzeug_file_class, FileStorage) is False:
+            raise Exception("werkzeug_file_class should be instance of FileStorage")
 
-    if secure_fname and use_original_filename:
-        # Example: URzRykJFOiSCCsQo-my-og-filename.png
-        return '{}-{}{}'.format(random_fname, secure_fname[:18], ext)
+        self.file = werkzeug_file_class
+        self.custom_filename = None
+        self.use_original_filename = use_original_filename
 
-    return '{}{}'.format(random_fname, ext)
+        # splitext returns tuple ('my-file', '.txt')
+        self._filename_tuple = os.path.splitext(self.file.filename.lower())
+        self.original_filename = self._filename_tuple[0]
+        self.extension = self._filename_tuple[1]
 
-def create_hmac_hash(secret, hmac_data) -> str:
-    """
-    Creates a hash using `hmac.new` and returns it
+    def get_filename(self, nbytes=12) -> str:
+        """
+        Returns custom filename, generated using `secrets.token_urlsafe`
+        """
+        if self.custom_filename is None:
+            custom_filename = secrets.token_urlsafe(nbytes)
 
-    :param str secret: The secret key for the hash
-    :param str hmac_data: The message/input for the hash
-    :return str hmac_hash HMAC hash
-    """
-    secret = secret.encode('utf-8')
-    hmac_data = hmac_data.encode('utf-8')
-    hmac_hash = hmac.new(secret, hmac_data, hashlib.sha256).hexdigest()
-    return hmac_hash
+            if self.use_original_filename:
+                secure_og_filename = secure_filename(self.original_filename[:18])
+                self.custom_filename = '{}-{}{}'.format(custom_filename, secure_og_filename, self.extension)
+                return self.custom_filename
 
-def is_valid_hash(hash_a: str, hash_b: str) -> bool:
-    """
-    Compares two hashes using `hmac.compare_digest`
-    """
-    return hmac.compare_digest(hash_a, hash_b)
+            self.custom_filename = '{}{}'.format(custom_filename, self.extension)
 
-def sharex_config():
-    """
-    Returns the configuration for ShareX as dictionary
-    """
-    cfg = {
-        "Version": "1.0.0",
-        "DestinationType": "ImageUploader, FileUploader",
-        "RequestMethod": "POST",
-        "RequestURL": flask.url_for('api.upload', _external=True),
-        "Body": "MultipartFormData",
-        "FileFormName": "file",
-        "URL": "$json:url$",
-        "DeletionURL": "$json:delete_url$",
-        "Headers": {
-            "Authorization": "YOUR-UPLOAD-PASSWORD-HERE",
-            "X-Use-Original-Filename": 1,
-        },
-        "ErrorMessage": "$json:status$"
-    }
+        return self.custom_filename
+    
+    def is_allowed(self) -> bool:
+        """
+        Check if file is allowed, based on `config.ALLOWED_EXTENSIONS`
+        """
+        return self.extension in config.ALLOWED_EXTENSIONS
 
-    return cfg
+    def save(self, save_directory = config.UPLOAD_DIR) -> None:
+        """
+        Saves the file to `UPLOAD_DIR`
+        """
+        if os.path.isdir(save_directory) is False:
+            os.makedirs(save_directory)
+
+        save_path = safe_join(save_directory, self.get_filename())
+        self.file.save(save_path)
+
+    @staticmethod
+    def delete(filename: str) -> bool:
+        """
+        Deletes the file from `config.UPLOAD_DIR`, if it exists
+        """
+        file_path = safe_join(config.UPLOAD_DIR, filename)
+
+        if os.path.isfile(file_path) is False:
+            return False
+
+        os.remove(file_path)
+
+        return True
+    
+    @staticmethod
+    def create_hmac_hash(filename: str, secret_key: str = None) -> str:
+        """
+        Creates HMAC hash using the filename and returns it
+        """
+
+        hmac_hash = hmac.new(
+            secret_key.encode('utf-8'), 
+            filename.encode('utf-8'),
+            hashlib.sha256
+        ).hexdigest()
+
+        return hmac_hash
+
+    @staticmethod
+    def is_valid_hash(hash_a: str, hash_b: str) -> bool:
+        """
+        Compares two hashes using `hmac.compare_digest`
+        """
+        return hmac.compare_digest(hash_a, hash_b)
+    
+    @staticmethod
+    def sharex_config() -> dict:
+        """
+        Returns the configuration for ShareX as dictionary
+        """
+        cfg = {
+            "Version": "1.0.0",
+            "DestinationType": "ImageUploader, FileUploader",
+            "RequestMethod": "POST",
+            "RequestURL": flask.url_for('api.upload', _external=True),
+            "Body": "MultipartFormData",
+            "FileFormName": "file",
+            "URL": "$json:url$",
+            "DeletionURL": "$json:delete_url$",
+            "Headers": {
+                "Authorization": "YOUR-UPLOAD-PASSWORD-HERE",
+                "X-Use-Original-Filename": 1,
+            },
+            "ErrorMessage": "$json:status$"
+        }
+
+        return cfg
