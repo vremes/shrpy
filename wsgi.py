aa72bf83b05e27ced003197252cc9ad33c4caf0e
vremes <valtteriremes@outlook.com>
2021-02-09 21:54:23 +0200

    Don't bind to all interfaces.

--- wsgi.py
+++ wsgi.py
@@ -11,4 +11,4 @@ if __name__ == '__main__':
     application.secret_key = 'dev'
     application.debug = True
     application.config['DEBUG'] = True
-    application.run(host='0.0.0.0', port=5000)
+    application.run()
