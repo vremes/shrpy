0319f5f8d6bcbe49d3cca7e5d30b174f85cef369
vremes <valtteriremes@outlook.com>
2021-06-09 19:07:39 +0300

    Override DiscordWebhook.execute method.

--- app/helpers/discord.py
+++ app/helpers/discord.py
@@ -1,15 +1,13 @@
 # standard library imports
-import logging
 from random import randint
 
 # pip imports
+from flask import current_app
 from requests.exceptions import Timeout
 from discord_webhook import DiscordWebhook, DiscordEmbed
 
 # local imports
 from app.helpers.utils import Message
-
-logger = logging.getLogger('discord_webhook')
 
 class CustomDiscordWebhook(DiscordWebhook):
     def __init__(self, url=None, **kwargs):
@@ -20,18 +18,15 @@ class CustomDiscordWebhook(DiscordWebhook):
         """Checks if Discord webhook is enabled."""
         return self.url is not None and len(self.url) > 0
 
-    def send(self) -> None:
-        """Executes the webhook and handles Timeout exception"""
+    def execute(self, remove_embeds=True, remove_files=True):
+        """Executes the webhook and handles Timeout exception."""
         if self.is_enabled is False:
             return None
 
         try:
-            self.execute(remove_embeds=True)
-        except Timeout as e:
-            logger.error('requests.exceptions.Timeout exception has occurred: {} - {}'.format(
-                e.request.method,
-                e.request.url
-            ))
+            return super().execute(remove_embeds, remove_files)
+        except Timeout as err:
+            current_app.logger.error(f'requests.exceptions.Timeout exception has occurred during webhook execution: {err}')
 
 class CustomDiscordEmbed(DiscordEmbed):
     def __init__(self, **kwargs):
