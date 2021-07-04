ca606f96386a55742c43f80abaa3004e42e8a54c
vremes <valtteriremes@outlook.com>
2021-07-04 20:59:22 +0300

    Get configuration values from OS environment.

--- app/config.py
+++ app/config.py
@@ -1,45 +1,43 @@
 import os
 
-# Maximum upload file size (~16mb)
-# Example: https://flask.palletsprojects.com/en/1.1.x/patterns/fileuploads/#improving-uploads
-MAX_CONTENT_LENGTH: int = 16 * 1024 * 1024
+# Maximum upload file size, defaults to ~16mb
+MAX_CONTENT_LENGTH = int(os.getenv('MAX_CONTENT_LENGTH', 16 * 1024 * 1024))
 
-# Path for uploaded files
-UPLOAD_DIR: str = os.path.join(os.getcwd(), 'app', 'uploads')
+# Path for uploaded files, defaults to /app/uploads/
+UPLOAD_DIR = os.getenv('UPLOAD_DIR', os.path.join(os.getcwd(), 'app', 'uploads'))
 
-# List of allowed file extensions
-ALLOWED_EXTENSIONS: list = ['.png', '.jpg', '.jpeg', '.gif', '.webm', '.mp4', '.webp', '.txt', '.m4v']
+# List of allowed file extensions, defaults to ['png', 'jpg', 'jpeg', 'gif', 'webm', 'mp4', 'webp', 'txt', 'm4v']
+ALLOWED_EXTENSIONS = os.getenv('ALLOWED_EXTENSIONS', 'png;jpg;jpeg;gif;webm;mp4;webp;txt;m4v').split(';')
 
-# Password for file uploads, leave it to empty string if you don't want to use a password at all
-UPLOAD_PASSWORD: str = ''
+# Password for file uploads, defaults to None
+UPLOAD_PASSWORD = os.getenv('UPLOAD_PASSWORD')
 
-# List of Discord webhooks https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks
-# leave this to empty list if you don't want to use Discord webhooks
-DISCORD_WEBHOOKS: list = []
+# List of Discord webhooks, defaults to empty list
+DISCORD_WEBHOOKS = os.getenv('DISCORD_WEBHOOKS', '').split(';')
 
-# Timeout for discord webhook, in seconds
-DISCORD_WEBHOOK_TIMEOUT: int = 5
+# Timeout for discord webhook in seconds, defaults to 5
+DISCORD_WEBHOOK_TIMEOUT = int(os.getenv('DISCORD_WEBHOOK_TIMEOUT', 5))
 
-# The amount of bytes python-magic will read from uploaded file to determine its extension
-MAGIC_BUFFER_BYTES: int = 2048
+# The amount of bytes python-magic will read from uploaded file to determine its extension, defaults to 2048
+MAGIC_BUFFER_BYTES = int(os.getenv('MAGIC_BUFFER_BYTES', 2048))
 
-# The amount of bytes for secrets.token_urlsafe, used in filenames
-FILE_TOKEN_BYTES: int = 12
+# The amount of bytes for secrets.token_urlsafe, used in filenames, defaults to 12
+FILE_TOKEN_BYTES = int(os.getenv('FILE_TOKEN_BYTES', 12))
 
-# The amount of bytes for secrets.token_urlsafe, used in shortened URLs
-URL_TOKEN_BYTES: int = 6
+# The amount of bytes for secrets.token_urlsafe, used in shortened URLs, defaults to 6
+URL_TOKEN_BYTES = int(os.getenv('URL_TOKEN_BYTES', 6))
 
-# The amount of characters that will be appended to random filename from original filename when X-Use-Original-Filename header value is set to 1
-ORIGINAL_FILENAME_LENGTH: int = 18
+# The amount of characters which will be appended to random filename from original filename, defaults to 18
+ORIGINAL_FILENAME_LENGTH = int(os.getenv('ORIGINAL_FILENAME_LENGTH', 18))
 
-# Filename for log file
-LOGGER_FILE_NAME: str = 'shrpy.log'
+# Filename for log file, defaults to shrpy.log
+LOGGER_FILE_NAME = os.getenv('LOGGER_FILE_NAME', 'shrpy.log')
 
-# Path for log file
-LOGGER_FILE_PATH: str = os.path.join(os.getcwd(), 'app', 'logs')
+# Path for log file, defaults to /app/logs/
+LOGGER_FILE_PATH = os.getenv('LOGGER_FILE_PATH', os.path.join(os.getcwd(), 'app', 'logs'))
 
-# The maximum size for log file in bytes
-LOGGER_MAX_BYTES: int = 8 * 1024 * 1024
+# The maximum size for log file in bytes, defaults to ~8mb
+LOGGER_MAX_BYTES = int(os.getenv('LOGGER_MAX_BYTES', 8 * 1024 * 1024))
 
-# The amount of log files to backup
-LOGGER_BACKUP_COUNT: int = 5
+# The amount of log files to backup, defaults to 5
+LOGGER_BACKUP_COUNT = int(os.getenv('LOGGER_BACKUP_COUNT', 5))
