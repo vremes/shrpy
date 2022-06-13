8023598d5c548ad3db24365b5dc9be72489f9f52
vremes <valtteriremes@outlook.com>
2022-06-14 01:07:34 +0300

    Remove application.secret_key since it isn't needed anymore.

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,8 @@
-from os import getenv
 from app import create_app
 
 # WSGI instance
 application = create_app()
 
-# Get secret from OS environment
-application.secret_key = getenv('FLASK_SECRET')
-
 if __name__ == '__main__':
-    application.secret_key = 'dev'
     application.debug = True
     application.run()
